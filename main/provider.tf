59e718c11c54b309c75816949171cfd8539281b1
Uyên Thy <uynthyee.313@gmail.com>
2025-10-22 17:32:54 +0700

    feat(vpc): add module vpc

---
+++ main/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicop/aws"
+        version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "UITGO-state-bucket"
+    key = "UITGO/terraform.tfstate"
+    region = "ap-southeast-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      Project = "UITGO"
+      Environment = var.environment
+      ManagedBy = "Terraform"
+    }
+  }
+}
